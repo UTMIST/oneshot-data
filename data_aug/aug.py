dc793483f8d6c6b9df62f9c148495c987391332f
Varun Lodaya <varunblodaya@gmail.com>
2020-11-08 20:30:27 -0500

    Added a proper data augmentation method

--- data_aug/aug.py
+++ data_aug/aug.py
@@ -1,23 +1,44 @@
 import torch
 import torchvision.transforms as transforms
-from torchvision.utils import save_image
-from customDataset import wordDataset
-
-
-my_transforms = transforms.Compose([
-    transforms.ToPILImage(),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=45),
-    # transforms.RandomCrop((224, 224)) , #size?
-    transforms.RandomVerticalFlip(p=0.05),
-    transforms.ToTensor()
-])
-
-dataset = wordDataset(csv_file = './csv/Angelic.csv', root_dir = 'images_evaluation/Angelic', transform=my_transforms)
-
-n = 1
-for i in range(10):
-    for img, label in dataset:
-        save_image(img, 'tf_'+str(n)+'.png')
-        n += 1
+# from torchvision.utils import save_image
+# from customDataset import wordDataset
+import os
+import random
+from PIL import Image
         
+def main():
+    random.seed(311420)
+    root = os.getcwd()
+    # print(root)
+    root = os.path.join(root, 'data/images_background/')
+
+    NUM_IMAGES_AUGMENTED = 5 # Number of images per character augmented
+    NUM_AUGMENTATIONS = 5  # Number of times each image is augmented
+    
+
+    transform = transforms.Compose([
+        transforms.RandomAffine(degrees=180, translate=(0.15,0.15), fillcolor=255, shear=60)
+    ])
+
+    for language in os.listdir(root):
+        language_path = os.path.join(root, language)
+        for character in os.listdir(language_path):
+            character_path = os.path.join(language_path, character)
+            images = os.listdir(character_path)
+            sampled_images = random.sample(images, NUM_IMAGES_AUGMENTED)
+            for i, sampled_image in enumerate(sampled_images):
+                image_name = sampled_image[:-4]
+                image = Image.open(os.path.join(character_path, sampled_image))
+                # print(character_path)
+                for j in range(1, NUM_AUGMENTATIONS+1):
+                    transformed_image = transform(image)
+                    # print("{}/{}_aug{}.png".format(os.path.join(character_path, sampled_image), image_name, j))
+                    transformed_image.save("{}/{}_aug{}.png".format(character_path, image_name, j))
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
